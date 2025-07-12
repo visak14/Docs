f8ecd565b95e1a6a9016a9a66307839f467ecba6
Vishal <121089328+visak14@users.noreply.github.com>
2025-07-12 16:14:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,7 @@ const startServer = async () => {
       throw new Error("MONGO_URI is not defined in .env");
     }
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log(" MongoDB connected");
 
